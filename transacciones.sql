ee0a873c1368b21393e1aec9bbb8114cfe25ff60
jadevillavicencio <jade.villavicencio@ucsp.edu.pe>
2026-06-28 23:22:25 -0500

    Modify invoice insertion to use LASTVAL()
    Updated the invoice insertion to use LASTVAL() for dynamic ID retrieval.

--- transacciones.sql
+++ transacciones.sql
@@ -1,5 +1,3 @@
--- TRANSACCIÓN REGISTRAR PEDIDO  (4 TABLAS)
-
 
 BEGIN;
 
@@ -15,16 +13,13 @@ UPDATE MESA SET estado = 'Ocupada' WHERE id_mesa = 3;
 
 COMMIT;
 
--- TRANSACCIÓN REGISTRAR PAGO (4 TABLAS)
-
-
 BEGIN;
 
 INSERT INTO PAGO (monto, metodo, fcha_pago, estado, id_pedido)
 VALUES (15000, 'tarjeta debito', CURRENT_DATE, 'completado', 5);
 
 INSERT INTO FACTURA (numero, fecha_emision, monto_total, id_pago)
-VALUES ('FAC-001', CURRENT_DATE, 15000, 1);
+VALUES ('FAC-001', CURRENT_DATE, 15000, LASTVAL());
 
 UPDATE PEDIDO SET estado = 'Pagado' WHERE id_pedido = 5;
 
