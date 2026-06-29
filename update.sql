7497666f75386695baf224484d54eecdbaf37eb6
jadevillavicencio <jade.villavicencio@ucsp.edu.pe>
2026-06-28 23:21:18 -0500

    Fix SQL updates and modify CLIENTE details
    Corrected SQL syntax and updated client information.

--- update.sql
+++ update.sql
@@ -1,11 +1,16 @@
+
 UPDATE SUCURSAL 
-SET nombre='Sucursal Habanas',direccion='Av.Bellavista 432'
+SET nombre='Sucursal Habanas', direccion='Av. Bellavista 432'
 WHERE ciudad='Arequipa';
 
-UPDATE PEDIDO
-SET id_empleado= 2, id_mesa= 4, estado='Pendiente', total=150
-WHERE id_cliente=4;
+UPDATE CLIENTE
+SET nombre='Jose', apellido='Velasquez'
+WHERE dni='12345678';
 
 UPDATE PROVEEDOR
 SET nombre='Tyler', ruc='20552103816', telefono='598005744', email='Tlr_937@gmail.com'
-WHERE id_scrsal='4';
+WHERE id_scrsal=4;
+
+UPDATE PEDIDO
+SET id_empleado=2, id_mesa=4, estado='Pendiente', total=150
+WHERE id_cliente=3;
